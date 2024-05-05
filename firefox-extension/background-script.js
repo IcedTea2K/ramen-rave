7b89d71ead38e9bc916871e88b7e3b498420118f
Minh Au <auduongminh2004@gmail.com>
2024-05-04 22:21:01 -0400

    connect content-script with background-script

--- firefox-extension/background-script.js
+++ firefox-extension/background-script.js
@@ -1,8 +1,17 @@
 import * as _ from "./wasm_exec.js";
 
 console.log("Running background script")
+// Fetch and run Go wasm
 const go = new Go();
 
 WebAssembly.instantiateStreaming(fetch("bin/main.wasm"), go.importObject).then((result) => {
    go.run(result.instance);
 });
+
+// Listen and talk to the content script
+let contentCommPort;
+
+browser.runtime.onConnect.addListener((port) => {
+   contentCommPort = port;
+   contentCommPort.postMessage({ message: "You stinky" })
+})
--- firefox-extension/index.js
+++ firefox-extension/index.js
@@ -1,1 +1,6 @@
 console.log("Extension entry")
+let port = browser.runtime.connect({ name: "bg-comm"})
+
+port.onMessage.addListener((msg) =>{
+   console.log(msg)
+})
