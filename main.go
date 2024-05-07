6bf420accbc95efa743e0832497bad10d25287f9
Minh Au <auduongminh2004@gmail.com>
2024-05-07 00:46:47 -0400

    manipulate DOM with go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+//go:build js && wasm
 package main
 
-import "fmt"
+import (
+   "fmt"
+   "syscall/js"
+)
 
 func main()  {
    fmt.Println("Oh Yeahhhh, webassembly")
+
+   document := js.Global().Get("document")
+   body := document.Get("body")
+   style := body.Get("style")
+   style.Set("border", "5px solid green")
+   fmt.Println("Done changing doc")
 }
